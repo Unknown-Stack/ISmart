0ca7ebef62472b0ccc52bdf26a041d3c5f426b70
Danil <iknownstack@gmail.com>
2021-06-19 14:50:21 +0300

    final

---
+++ keys/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGODB_URI: `mongodb+srv://danil:78984567@cluster0.obl42.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  SESSION_SECRET: "some secret value",
+};
---
+++ public/app.js
@@ -0,0 +1,100 @@
+const toCurrency = (price) => {
+  return new Intl.NumberFormat("ru-RU", {
+    currency: "rub",
+    style: "currency",
+  }).format(price);
+};
+
+const toDate = (date) => {
+  return new Intl.DateTimeFormat("ru-RU", {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  }).format(new Date(date));
+};
+
+document.querySelectorAll(".price").forEach((node) => {
+  node.textContent = toCurrency(node.textContent);
+});
+
+document.querySelectorAll(".date").forEach((node) => {
+  node.textContent = toDate(node.textContent);
+});
+
+const $card = document.querySelector("#card");
+if ($card) {
+  $card.addEventListener("click", (event) => {
+    if (event.target.classList.contains("js-remove")) {
+      const id = event.target.dataset.id;
+      const csrf = event.target.dataset.csrf;
+
+      fetch("/card/remove/" + id, {
+        method: "delete",
+        headers: {
+          "X-XSRF-TOKEN": csrf,
+        },
+      })
+        .then((res) => res.json())
+        .then((card) => {
+          if (card.courses.length) {
+            const html = card.courses
+              .map((c) => {
+                return `
+              <tr>
+                <td>${c.title}</td>
+                <td>${c.count}</td>
+                <td>
+                  <button class="btn btm-small js-remove" data-id="${c.id}">Удалить</button>
+                </td>
+              </tr>
+              `;
+              })
+              .join("");
+            $card.querySelector("tbody").innerHTML = html;
+            $card.querySelector(".price").textContent = toCurrency(card.price);
+          } else {
+            $card.innerHTML = "<p>Корзина пуста</p>";
+          }
+        });
+    }
+  });
+}
+
+M.Tabs.init(document.querySelectorAll(".tabs"));
+
+const slides = document.querySelectorAll(".slide");
+
+for (const slide of slides) {
+  slide.addEventListener("click", () => {
+    clearActiveClasses();
+    slide.classList.add("active");
+  });
+}
+
+function clearActiveClasses() {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+}
+
+document.querySelector("#search").oninput = function () {
+  let val = this.value.trim();
+  let card = document.querySelectorAll(".cards");
+  if (val !== "") {
+    card.forEach(function (elem) {
+      if (elem.innerText.search(val) == -1) {
+        elem.classList.add("hide");
+        console.log(card);
+      } else {
+        elem.classList.remove("hide");
+      }
+    });
+  } else if (val === "") {
+    card.forEach(function (elem) {
+      elem.classList.remove("hide");
+    });
+  }
+};
